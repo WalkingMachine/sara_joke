40abb1aebdd6c3dbc44f5442919f5eb10f8b7879
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2017-09-17 15:25:10 -0400

    Add yaml

--- src/joke.py
+++ src/joke.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 import rospy
 import roslib
+
+import yaml
+
 from random import shuffle
 from sara_joke.srv import joke
 
 
 def get_random_joke(req):
 
-    if req.language == "en":
-        jokes = [
-            "A blind man walks into a bar. And a table. And a chair.",
-            "Why was 6 afraid of 7?                  Because 7, 8, 9",
-            "Trust me i was build by futur engineers",
-            "A robot walks into a pharmacy. The pharmacist asks him if he'd like anything. The robot replies, A soul.",
-            "Two statisticians are out hunting when one of them sees a duck. The first takes aim and shoots, but the bullet goes sailing past six inches too high. The second statisticians also takes aim and shoots, but this time the bullet goes saling past six inches too low. The two statisticians then give one another high fives and exclaim, Got him!",
-        ]
-    elif req.language == "fr":
-        jokes = []
-    else:
-        rospy.logfatal("Wrong parameters")
-        rospy.logfatal("Wrong parameters")
-        return "Error"
+    with open(rospy.get_param("joke_"+req.language), 'r') as stream:
+        try:
+            jokes = yaml.load(stream=stream)
+
+        except yaml.YAMLError as exc:
+            rospy.logerr(exc)
 
     shuffle(jokes)
     return jokes[0]
